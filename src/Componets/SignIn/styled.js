1cbb73ba225439d93594014231bdefe6e2040022
orzumand <hayotbekyoldashev0@gmail.com>
2022-11-13 16:41:16 +0500

    Login page building

--- src/Componets/SignIn/styled.js
+++ src/Componets/SignIn/styled.js
@@ -9,13 +9,51 @@ const Left = styled.div`
   flex: 1;
   background-image: url(${img});
   background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 const Right = styled.div`
   flex: 1;
   background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
-const Img = styled.img`
-  height: 100vh;
-  width: 100%;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
 `;
-export { Container, Left, Right, Img };
+Logo.Img = styled.img`
+  width: ${({ width }) => (width ? `${width}px` : "150px")};
+`;
+Logo.Title = styled.div`
+  font-style: normal;
+  font-weight: 800;
+  font-size: ${({ fontSize }) => (fontSize ? `${fontSize}px` : "30px")};
+
+  text-transform: uppercase;
+
+  color: ${({ color }) => (color ? color : "#ffffff")};
+`;
+Logo.Title.Subtitle = styled.div`
+  font-weight: 500;
+
+  font-size: ${({ fontSize }) => (fontSize ? `${fontSize}px` : "20px")};
+`;
+
+const Title = styled.div`
+  font-style: normal;
+  font-weight: 800;
+  font-size: 23px;
+  line-height: 50px;
+  text-align: center;
+`;
+Title.Subs = styled.div`
+  font-weight: 300;
+  font-size: 13px;
+`;
+export { Container, Left, Right, Logo, Title };
